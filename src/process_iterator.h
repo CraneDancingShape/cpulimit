78e9e3c29493bf43118bbd9d729d8020e8d529d0
CraneDancingShape <ynobusawa@gmail.com>
2022-11-24 03:46:07 +0900

    fix: linux/kernel.h: split *_MAX and *_MIN macros into <linux/limits.h> header linux kernel 5.1 above

--- src/process_iterator.h
+++ src/process_iterator.h
@@ -25,6 +25,13 @@
 
 #include <unistd.h>
 #include <limits.h>
+#if defined(__LINUX___)
+    #include <linux/version.h>
+    #if LINUX_VERSION_CODE < KERNEL_VERSION(5,1,0)
+        #include <linux/limits.h>
+    #endif
+#endif
+
 #include <dirent.h>
 
 //USER_HZ detection, from openssl code
